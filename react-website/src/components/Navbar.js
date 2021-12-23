63dd03b0fbe6dcb9bcc1b0a259deb3cb5897560d
JOANITA-51 <nakityojoanita51@gmail.com>
2021-12-23 13:33:06 +0300

    button component

--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -9,34 +9,36 @@ function Navbar() {
          <nav className='navbar'>
             <div className='navbar-container'>
                 <Link to ="/" >
-                    NJR  <i className='fa-solid fa-face-grin-hearts'></i>
+                NJR  <i className='fa-solid fa-face-grin-hearts'></i>
                     <i className='fab fa-typo3'/>
-                    <div className='menu-icon' onClick={handleClick}>
-                        <i className={click? 'fas fa-times':'fas fa-bars'} />
-                    </div>
-                    <ul className={click? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to ='/' className='nav-links' onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to ='/services' className='nav-links' onClick={closeMobileMenu}>
-                                Services
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to ='/products' className='nav-links' onClick={closeMobileMenu}>
-                                Products
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to ='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
-                                Sign Up
-                            </Link>
-                        </li>
-                    </ul>
                 </Link>
+
+                <div className='menu-icon' onClick={handleClick}>
+                        <i className={click? 'fas fa-times':'fas fa-bars'} />
+                </div>
+                <ul className={click? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to ='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to ='/services' className='nav-links' onClick={closeMobileMenu}>
+                            Services
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to ='/products' className='nav-links' onClick={closeMobileMenu}>
+                            Products
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to ='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+                 </ul>
+               
             </div>
          </nav>
         </>
