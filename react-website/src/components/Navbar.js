dc174f2ecaada8d198f5305f4e0b616658eba50e
JOANITA-51 <nakityojoanita51@gmail.com>
2021-12-23 12:21:20 +0300

    creating a menu icon

--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom';
 function Navbar() {
+    const {click, setClick} = useState(false);
+    const handleClick = () => setClick(!click);
     return (
         <>
          <nav className='navbar'>
             <div className='navbar-container'>
                 <Link to ="/" >
                     NJR  <i className='fa-solid fa-face-grin-hearts'></i>
+                    <i className='fab fa-typo3'/>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click? 'fas fa-times':'fas fa-bars'} />
+                    </div>
                 </Link>
             </div>
          </nav>
