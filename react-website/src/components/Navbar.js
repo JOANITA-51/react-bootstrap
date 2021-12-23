e22f81e8413ab465dc0f647e82bbb181545e7539
JOANITA-51 <nakityojoanita51@gmail.com>
2021-12-23 20:00:19 +0300

    navigation bar completed

--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -1,17 +1,36 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
 function Navbar() {
-    const {click, setClick} = useState(false);
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+    
+    const showButton = ()=>{
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true); 
+        }
+    };
+
+    useEffect(()=>{
+        showButton();
+    }, [])
+
+    window.addEventListener('resize', showButton)
     return (
         <>
          <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to ="/" >
-                NJR  <i className='fa-solid fa-face-grin-hearts'></i>
-                    <i className='fab fa-typo3'/>
+                <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
+                NJRY   <i className='fab fa-typo3'/>
                 </Link>
+                
+                
 
                 <div className='menu-icon' onClick={handleClick}>
                         <i className={click? 'fas fa-times':'fas fa-bars'} />
@@ -37,8 +56,8 @@ function Navbar() {
                             Sign Up
                         </Link>
                     </li>
-                 </ul>
-               
+                </ul>
+               {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
             </div>
          </nav>
         </>
